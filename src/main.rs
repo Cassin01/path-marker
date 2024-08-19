bb788b93f08009189bb02a322897df0cee587edd
Cassin01 <koyojb@gmail.com>
2024-08-19 14:22:03 +0900

    update: Cargo.toml, main.rs,

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// #![allow(unused)]
+use clap::arg_enum;
 use clap::Parser;
 use std::collections::HashMap;
 use std::env;
@@ -6,9 +6,9 @@ use std::io::prelude::*;
 use std::io::Read;
 use std::io::Write;
 use std::os::unix::ffi::OsStrExt;
-use std::path::PathBuf;
+use std::path::Path;
 
-extern crate confy;
+extern crate structopt;
 extern crate serde_derive;
 use serde_derive::{Deserialize, Serialize};
 
@@ -41,7 +41,7 @@ fn head_border(len: usize, d: usize) -> usize {
 }
 fn enqueue(path: &[u8], hist: &str, cfg: ConfyConfig) -> Result<(), BErr> {
     let mut file = std::fs::OpenOptions::new().read(true).open(hist)?;
-    file.rewind()?;
+    file.rewind()?; // reset file cursor to the beginning
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
@@ -55,13 +55,16 @@ fn enqueue(path: &[u8], hist: &str, cfg: ConfyConfig) -> Result<(), BErr> {
         .map(|x| (x, post_inc!(cnt)))
         .collect::<HashMap<&str, usize>>();
 
-    let mut contents_list = contents_set.into_iter().collect::<Vec<(&str, usize)>>();
+    let mut contents_list = contents_set
+        .into_iter()
+        .filter(|x| Path::new(x.0).exists())
+        .collect::<Vec<(&str, usize)>>();
     contents_list.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
 
     let contents_sliced = &contents_list[head_border(contents_list.len(), cfg.history_max_size)..];
 
     let contents_buf = contents_sliced
-        .into_iter()
+        .iter()
         .map(|x| x.0)
         .collect::<Vec<&str>>()
         .join("\n");
@@ -76,16 +79,27 @@ fn enqueue(path: &[u8], hist: &str, cfg: ConfyConfig) -> Result<(), BErr> {
     Ok(())
 }
 
+arg_enum! {
+    #[derive(Debug)]
+    /// Run the command based on the pattern
+    enum Patt {
+        Mark,
+        Show,
+        Conf,
+        Clean,
+        Edit
+    }
+}
+
 #[derive(Parser)]
 struct Cli {
-    pattern: String,
-    // #[clap(parse(from_os_str))]
-    // path: std::path::PathBuf,
+    #[structopt(possible_values = Patt::variants(), case_insensitive = true)]
+    pattern: Patt,
 }
 
 type BErr = Box<dyn std::error::Error>;
 
-pub fn path_buf_to_u8_slice_unix(input: &PathBuf) -> &[u8] {
+pub fn path_buf_to_u8_slice_unix(input: &Path) -> &[u8] {
     input.as_os_str().as_bytes()
 }
 
@@ -98,7 +112,6 @@ fn mark(_args: Cli, cfg: ConfyConfig) -> Result<(), BErr> {
     enqueue(path_buf_to_u8_slice_unix(&path), &hist, cfg)?;
     Ok(())
 }
-
 fn show(_args: Cli) -> Result<(), BErr> {
     let hist = get_hist_path()?;
     if std::path::Path::new(&hist).exists() {
@@ -132,13 +145,59 @@ fn conf(cfg: ConfyConfig) -> Result<(), BErr> {
     Ok(())
 }
 
+fn clean() -> Result<(), BErr> {
+    let hist = get_hist_path()?;
+    if !Path::new(&hist).exists() {
+        println!("No path are registered yet.");
+        return Ok(());
+    }
+    let mut file = std::fs::OpenOptions::new().read(true).open(&hist)?;
+    file.rewind()?; // reset file cursor to the beginning
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let contents_list = contents.lines().collect::<Vec<_>>();
+
+    let mut new_contents_list: Vec<&str> = Vec::new();
+    for content in contents_list.iter() {
+        if Path::new(content).exists() {
+            new_contents_list.push(content);
+        }
+    }
+
+    let contents_buf = new_contents_list.join("\n");
+
+    let mut file = std::fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(hist)?;
+    file.rewind()?;
+    file.write_all(contents_buf.as_bytes())?;
+
+    Ok(())
+}
+
+fn edit() -> Result<(), BErr> {
+    let hist = get_hist_path()?;
+    if !Path::new(&hist).exists() {
+        println!("No path are registered yet.");
+        return Ok(());
+    }
+    let editor = env::var("EDITOR").unwrap_or("vim".to_string());
+    let status = std::process::Command::new(editor).arg(hist).status()?;
+    if !status.success() {
+        return Err(Box::from("Failed to open the editor"));
+    }
+    Ok(())
+}
+
 fn main() -> Result<(), BErr> {
     let cfg: ConfyConfig = confy::load("path-marker", None)?;
     let args = Cli::parse();
-    match &*args.pattern {
-        "mark" => mark(args, cfg),
-        "show" => show(args),
-        "conf" => conf(cfg),
-        _ => panic!("There was a problem reading arguments"),
+    match args.pattern {
+        Patt::Mark => mark(args, cfg),
+        Patt::Show => show(args),
+        Patt::Conf => conf(cfg),
+        Patt::Clean => clean(),
+        Patt::Edit => edit(),
     }
 }
